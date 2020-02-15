e1be714f131f1d98b4741a72c207afb4d51c25d7
andreeamatei <githubC0nt>
2020-02-15 14:40:59 +0200

    Add helper methods example

--- src/main/java/HelperMethods.java
+++ src/main/java/HelperMethods.java
@@ -1,2 +1,22 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class HelperMethods {
+
+    public static final String H1_TAGNAME = "h1";
+
+    @FindBy(tagName = H1_TAGNAME)
+    WebElement title;
+
+    public HelperMethods(WebDriver driver)
+    {
+        PageFactory.initElements(driver, this);
+    }
+
+    public String getTitleText()
+    {
+        return title.getText();
+    }
 }
